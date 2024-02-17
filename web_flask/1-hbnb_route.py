b89e02b21d5b22998ba0665e2c978925b7fe7cea
Desire-2 <122997271+Desire-2@users.noreply.github.com>
2024-02-17 21:36:14 +0200

    1-hbnb_route.py

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -2,16 +2,13 @@
 """
 starts a Flask web application
 """
-
 from flask import Flask
 app = Flask(__name__)
-
 
 @app.route('/', strict_slashes=False)
 def index():
     """returns Hello HBNB!"""
     return 'Hello HBNB!'
-
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
